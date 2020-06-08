0ce1abc1e19775126816eb7ea15109f34f0bf0c6
alexheckmann <39348705+alexheckmann@users.noreply.github.com>
2020-06-08 15:58:25 +0200

    fixed error handling

--- DocToPdfConverter.py
+++ DocToPdfConverter.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from _ctypes import COMError
 
@@ -8,18 +9,18 @@ def doc_to_pdf(_in, _out):
     pdf_format = 17
     file_in = os.path.abspath(_in)
     file_out = os.path.abspath(_out)
-    worddoc = comtypes.client.CreateObject('Word.Application')
+    
     try:
+        worddoc = comtypes.client.CreateObject('Word.Application')
         doc = worddoc.Documents.open(file_in)
         doc.SaveAs(file_out, FileFormat=pdf_format)
         doc.Close()
+        worddoc.Quit()
     except COMError:
         print("Could not read file:" + str(file_in))
-    finally:
-        worddoc.Quit()
+    
 
-
-destination = "C:\\Users\\ah\\Dropbox\\Dateien\\Bewerbungsunterlagen\\Motivationsschreiben\\Praktisches Studiensemester"
+destination = sys.argv[1]
 for file in os.listdir(destination):
     in_filename = destination + "\\" + file
     file_extension = os.path.splitext(in_filename)[-1].lower()
