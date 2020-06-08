ee3597686ba3449d7e008b9afbd694ae2ad2260c
alexheckmann <ah@ecommunications.de>
2020-06-08 15:37:36 +0200

    Added automatic docx to pdf converter

---
+++ DocToPdfConverter.py
@@ -0,0 +1,31 @@
+import os
+from _ctypes import COMError
+
+import comtypes.client
+
+
+def doc_to_pdf(_in, _out):
+    pdf_format = 17
+    file_in = os.path.abspath(_in)
+    file_out = os.path.abspath(_out)
+    worddoc = comtypes.client.CreateObject('Word.Application')
+    try:
+        doc = worddoc.Documents.open(file_in)
+        doc.SaveAs(file_out, FileFormat=pdf_format)
+        doc.Close()
+    except COMError:
+        print("Could not read file:" + str(file_in))
+    finally:
+        worddoc.Quit()
+
+
+destination = "C:\\Users\\ah\\Dropbox\\Dateien\\Bewerbungsunterlagen\\Motivationsschreiben\\Praktisches Studiensemester"
+for file in os.listdir(destination):
+    in_filename = destination + "\\" + file
+    file_extension = os.path.splitext(in_filename)[-1].lower()
+
+    if file_extension == ".docx":
+        out_filename = destination + "\\" + str(file).rstrip(".docx") + ".pdf"
+        doc_to_pdf(in_filename, out_filename)
+    else:
+        continue
