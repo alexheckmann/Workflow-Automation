644c32d7e26a3bea21575085adcb268236a4153e
alexheckmann <ah@ecommunications.de>
2020-06-09 15:12:07 +0200

    added exception handling

--- DocToPdfConverter.py
+++ DocToPdfConverter.py
@@ -37,26 +37,26 @@ def doc_to_pdf(_in, _out):
 
 @retry(5)
 def batch_conversion():
-    if len(sys.argv) > 1:
+    try:
         destination = sys.argv[1]
-    else:
+    except IndexError:
         destination = input("Please enter the destination folder:\n")
 
-        for file in os.listdir(destination):
-            in_filename = destination + "\\" + file
-            file_extension = os.path.splitext(in_filename)[-1].lower()
-            out_filename = destination + "\\" + str(file).rstrip(".docx") + ".pdf"
+    for file in os.listdir(destination):
+        in_filename = destination + "\\" + file
+        file_extension = os.path.splitext(in_filename)[-1].lower()
+        out_filename = destination + "\\" + str(file).rstrip(".docx") + ".pdf"
 
-            if ".docx" in file_extension:
-                pdf_filename = str(file).rstrip(".docx") + ".pdf"
-                pdf_exists = pdf_filename in os.listdir(destination)
-                if pdf_exists:
-                    print("PDF version of \"" + str(file) + "\" already exists")
-                else:
-                    doc_to_pdf(in_filename, out_filename)
-                    print("PDF version of " + str(file) + " created")
+        if ".docx" in file_extension:
+            pdf_filename = str(file).rstrip(".docx") + ".pdf"
+            pdf_exists = pdf_filename in os.listdir(destination)
+            if pdf_exists:
+                print("PDF version of \"" + str(file) + "\" already exists")
             else:
-                continue
+                doc_to_pdf(in_filename, out_filename)
+                print("PDF version of " + str(file) + " created")
+        else:
+            continue
 
 
 batch_conversion()
